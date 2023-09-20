cb5c38651b1db718f3afe0a304193e31937bf2dc
Maxime Langlois <mlangloi@c2r8p1.42lehavre.fr>
2023-09-20 12:25:43 +0200

    fix main.cpp

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -11,29 +11,55 @@
 /* ************************************************************************** */
 
 #include"MutantStack.hpp"
+#include<iterator>
 
 int main()
 {
-MutantStack<int> mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
-//[...]
-mstack.push(0);
-MutantStack<int>::it it = mstack.begin();
-MutantStack<int>::it ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::it it = mstack.begin();
+	MutantStack<int>::it ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	.
+	std::cout << std::endl;
+	
+	std::list<int> ls;
+	ls.push_back(5);
+	ls.push_back(17);
+	std::cout << ls.back() << std::endl;
+	ls.pop_back();
+	std::cout << ls.size() << std::endl;
+	ls.push_back(3);
+	ls.push_back(5);
+	ls.push_back(737);
+	//[...]
+	ls.push_back(0);
+	std::list<int>::iterator itr = ls.begin();
+	std::list<int>::iterator iter = ls.end();
+	++itr;
+	--itr;
+	while (itr != iter)
+	{
+		std::cout << *itr << std::endl;
+		++itr;
+	}
+	std::list<int> s2(ls);
+	
+	return 0;
 }
